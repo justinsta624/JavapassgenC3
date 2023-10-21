70cd52577f16f4e9fb7f365b3b882a9fecad30fa
justinsta624 <justinsta624@gmail.com>
2023-10-20 23:03:49 -0400

    Use CONST/LET/ParseInt/prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,16 @@
-// Assignment code here
+// Generate a random password based on criteria below;
+function generatePassword(){
+// Whether to include lower/uppercase, numbers, and/or special characters in random password  
+  const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numbers = '0123456789';
+  const specialC = '~!@#$%^&*()-_=+[{]}",<.>?/';
+
+// Prompt for password choosing criteria;
+let length = parseInt(prompt('Choose between 8 to 128 characters for the password:'));
+while (isNaN(length) || length < 8 || length > 128)
+    length = parseInt(prompt('Invalid input; Choose between 8 to 128 characters for the password:'));
+}
 
 
 // Get references to the #generate element
